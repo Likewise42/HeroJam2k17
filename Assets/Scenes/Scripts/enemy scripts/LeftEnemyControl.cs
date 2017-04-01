4ab36f338632efef5e2a112488f7878f5ce9b54f
Ryan Spencer <rxs2833@g.rit.edu>
2017-04-01 14:15:11 -0400

    Fixed everything

---
+++ Assets/Scenes/Scripts/enemy scripts/LeftEnemyControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftEnemyControl : MonoBehaviour {
+
+    public float moveSpeed;
+    private float inputDirection;
+    private float verticalVelocity;
+    private float gravity;
+    private float direction;
+    private Vector3 moveVector;
+    private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        gravity = 1.0f;
+        direction = -1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = 0;
+        }
+        else
+        {
+            verticalVelocity -= gravity;
+        }
+
+        inputDirection = direction * moveSpeed;
+
+        moveVector = new Vector3(inputDirection, verticalVelocity, 0);
+
+        controller.Move(moveVector * Time.deltaTime);
+	}
+
+    void changeDirection () {
+        direction = direction * -1;
+    }
+}
