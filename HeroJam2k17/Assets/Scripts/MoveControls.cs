db4cbd82da9e24ed81f9f9f45ff7ad21f74af3b9
Matthew Pressman <mrp3362@rit.edu>
2017-03-31 21:03:59 -0400

    Created Folder Structure and Added Movement/Camera Controls

---
+++ HeroJam2k17/Assets/Scripts/MoveControls.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sidescroller_MoveControls : MonoBehaviour {
+
+    public float moveSpeed;
+    private float inputDirection; //X Value of moveVector
+    private float verticalVelocity; //Y Value of moveVector
+    private float gravity;
+    private Vector3 moveVector;
+    private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        gravity = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        inputDirection = Input.GetAxis("Horizontal") * moveSpeed;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = 0;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                verticalVelocity = 20;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity;
+        }
+
+        moveVector = new Vector3(inputDirection, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
