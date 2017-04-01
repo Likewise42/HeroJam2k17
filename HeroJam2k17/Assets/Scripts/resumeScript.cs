298cb585b731eda5b1a369289aeb84846d6d404a
Ryan Spencer (RIT Student) <rxs2833@ad.rit.edu>
2017-04-01 10:43:04 -0400

    Ui Switching intially finished

--- HeroJam2k17/Assets/Scripts/resumeScript.cs
+++ HeroJam2k17/Assets/Scripts/resumeScript.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
-public class startMenu : MonoBehaviour, IPointerClickHandler
+public class resumeScript : MonoBehaviour, IPointerClickHandler
 {
 
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-    }
+		
+	}
 
     public void OnPointerClick(PointerEventData eventData)
     {
