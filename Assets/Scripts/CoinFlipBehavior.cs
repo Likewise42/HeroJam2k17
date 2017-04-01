3f1c998701fa0f1af43bb478715467f7a445401a
Ryan Spencer (RIT Student) <rxs2833@ad.rit.edu>
2017-04-01 16:42:30 -0400

    More UI Fixes, intial coin toss

---
+++ Assets/Scripts/CoinFlipBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CoinFlipBehavior : MonoBehaviour {
+
+    public GameObject coinSideA;
+    public GameObject coinSideB;
+    public GameObject noCoin;
+    private bool SideAactive = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space))
+        {
+            SideAactive = !SideAactive;
+            coinSideA.SetActive(SideAactive);
+            coinSideB.SetActive(!SideAactive);
+        }
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Pause");
+        }
+    }
+}
--- Assets/Scripts/exitScript.cs
+++ Assets/Scripts/exitScript.cs
@@ -1,23 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
-public class startMenu : MonoBehaviour, IPointerClickHandler
+public class exitScript : MonoBehaviour, IPointerClickHandler
 {
 
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-    }
-
+		
+	}
     public void OnPointerClick(PointerEventData eventData)
     {
-        SceneManager.LoadScene("coinUI");
+        Application.Quit();
     }
 }
